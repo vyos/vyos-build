c0cae8b7fec0d5e8a7c8ff9418397587e967fd15
Daniil Baturin <daniil@baturin.org>
2015-12-24 16:06:34 -0500

    Add configure script support for Debian mirror options.

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -2,3 +2,6 @@ import os
 
 BUILD_DIR = 'build'
 BUILD_CONFIG = os.path.join(BUILD_DIR, 'build-config.json')
+
+# The default mirror was chosen entirely at random
+DEBIAN_MIRROR = 'http://ftp.nl.debian.org/debian/'
