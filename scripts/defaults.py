53a878bb2d18a450f4096403d80461dd566ef29e
Daniil Baturin <daniil@baturin.org>
2016-12-21 22:28:17 +0100

    lb-config improvements: force firmware packages to be added to the image, and make security mirror configurable. XXX: need to find out how to make --security work too.

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -23,6 +23,7 @@ BUILD_CONFIG = os.path.join(BUILD_DIR, 'build-config.json')
 
 # The default mirror was chosen entirely at random
 DEBIAN_MIRROR = 'http://ftp.nl.debian.org/debian/'
+DEBIAN_SECURITY_MIRROR = 'http://security.debian.org/'
 
 DEBIAN_DISTRIBUTION = 'jessie'
 
