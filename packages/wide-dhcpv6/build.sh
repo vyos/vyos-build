747c86ad8cd2ba761612f89f395bb2454ed6975f
Christian Poessinger <christian@poessinger.com>
2021-04-09 12:53:31 +0200

    packages: wide-dhcpv6: rename build script

--- packages/wide-dhcpv6/build.sh
+++ packages/wide-dhcpv6/build.sh
@@ -2,24 +2,22 @@
 CWD=$(pwd)
 set -e
 
-WIDE_SRC=wide-dhcpv6
-
-if [ ! -d ${WIDE_SRC} ]; then
+SRC=wide-dhcpv6
+if [ ! -d ${SRC} ]; then
     echo "Source directory does not exists, please 'git clone'"
     exit 1
 fi
-
 
 PATCH_DIR=${CWD}/patches
 if [ -d $PATCH_DIR ]; then
     for patch in $(ls ${PATCH_DIR})
     do
         echo "I: Apply patch: ${patch} to main repository"
-        cp ${PATCH_DIR}/${patch} ${WIDE_SRC}/debian/patches/
-        echo ${patch} >> ${WIDE_SRC}/debian/patches/series
+        cp ${PATCH_DIR}/${patch} ${SRC}/debian/patches/
+        echo ${patch} >> ${SRC}/debian/patches/series
     done
 fi
 
-cd ${WIDE_SRC}
+cd ${SRC}
 echo "I: Build Debian Package"
 dpkg-buildpackage -uc -us -tc -b
