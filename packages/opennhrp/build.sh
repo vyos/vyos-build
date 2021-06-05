3766906e19a4cb61b573a7c19d77757fa588e2b5
Christian Poessinger <christian@poessinger.com>
2021-06-05 22:20:23 +0200

    opennhrp: remove first character from version number
    If not deployment of the package will fail with: Version field value
    'v0.14-20-g613277f': version number does not start with digit

--- packages/opennhrp/build.sh
+++ packages/opennhrp/build.sh
@@ -21,8 +21,10 @@ install --mode 0644 etc/racoon-ph1down.sh debian/etc
 install --strip --mode 0755 nhrp/opennhrp debian/usr/sbin
 install --strip --mode 0755 nhrp/opennhrpctl debian/usr/sbin
 
+# Version' field value 'v0.14-20-g613277f': version number does not start with digit
+# "cut" first character from version string
 fpm --input-type dir --output-type deb --name opennhrp \
-    --version $(git describe --always) --deb-compression gz \
+    --version $(git describe --always | cut -c2-) --deb-compression gz \
     --maintainer "VyOS Package Maintainers <maintainers@vyos.net>" \
     --description "NBMA Next Hop Resolution Protocol daemon" \
     --license "MIT" -C $INSTALL_DIR --package ..
