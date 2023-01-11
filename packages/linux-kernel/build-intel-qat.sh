1e59005e76ad39185c4d8c3c083e1a82fadc9a74
Christian Breunig <christian@breunig.cc>
2023-01-11 18:51:46 +0100

    qat: T4929: update Intel QAT drivers to 4.20.0-00001

--- packages/linux-kernel/build-intel-qat.sh
+++ packages/linux-kernel/build-intel-qat.sh
@@ -15,7 +15,7 @@ fi
 . ${KERNEL_VAR_FILE}
 
 declare -a intel=(
-    "http://dev.packages.vyos.net/source-mirror/QAT1.7.L.4.16.0-00017.tar.gz"
+    "https://dev.packages.vyos.net/source-mirror/QAT1.7.L.4.20.0-00001.tar.gz"
 )
 
 for url in "${intel[@]}"
