vyos/vyos-build

commit 3d5445d8f7c00f712963fbf85bbccaae13de92ea

T5499: arm64: build jool with correct depends
Signed-off-by: Date Huang <tjjh89017@hotmail.com>

packages/linux-kernel/build-jool.py
@@ -5,6 +5,10 @@ from requests import get
 from pathlib import Path
 from subprocess import run
 
+def find_arch() -> str:
+    import os
+    arch = os.system("dpkg-architecture -q DEB_HOST_ARCH").strip()
+    return arch
 
 # dependency modifier
 def add_depends(package_dir: str, package_name: str,
@@ -24,8 +28,9 @@ def add_depends(package_dir: str, package_name: str,
 
 
 # find kernel version and source path
+arch: str = find_arch()
 defaults_file: str = Path('../../data/defaults.toml').read_text()
-architecture_file: str = Path('../../data/architectures/amd64.toml').read_text()
+architecture_file: str = Path(f'../../data/architectures/{arch}.toml').read_text()
 KERNEL_VER: str = toml_loads(defaults_file).get('kernel_version')
 KERNEL_FLAVOR: str = toml_loads(architecture_file).get('kernel_flavor')
 KERNEL_SRC: str = Path.cwd().as_posix() + '/linux'

commit 28e63c2dcaf1b64aeb7928d29ca5b541413cf8f8

jool: T5499: fix AttributeError: 'int' object has no attribute 'strip'
This fixes an error introduced by commit 3d5445d8f7c0 ("T5499: arm64: build
jool with correct depends").

packages/linux-kernel/.gitignore
@@ -5,6 +5,7 @@
 /intel-qat
 /linux-firmware
 /ovpn-dco
+/jool*
 /qat*
 /QAT*
 *.tar.xz

packages/linux-kernel/build-jool.py
@@ -6,9 +6,8 @@ from pathlib import Path
 from subprocess import run
 
 def find_arch() -> str:
-    import os
-    arch = os.system("dpkg-architecture -q DEB_HOST_ARCH").strip()
-    return arch
+    tmp=run(['dpkg-architecture', '-q', 'DEB_HOST_ARCH'], capture_output=True)
+    return tmp.stdout.decode().strip()
 
 # dependency modifier
 def add_depends(package_dir: str, package_name: str,