7508677091abf2b9c92d9c5c393fb73bb5128aa8
Nicolas Vollmar <nvo@scaling.ch>
2024-08-07 06:55:39 +0200

    podman: T6635: add missing dependency on golang-github-containers-common
    This is required as the dependency will add /etc/containers/policy.json

--- packages/podman/build.sh
+++ packages/podman/build.sh
@@ -26,6 +26,7 @@ fpm --input-type dir --output-type deb --name podman \
     --version $VERSION --deb-compression gz \
     --maintainer "VyOS Package Maintainers <maintainers@vyos.net>" \
     --description "Engine to run OCI-based containers in Pods" \
-    --depends conmon --depends crun --depends netavark --depends libgpgme11 --depends fuse-overlayfs \
+    --depends conmon --depends crun --depends netavark --depends libgpgme11 \
+    --depends fuse-overlayfs --depends golang-github-containers-common \
     --license "Apache License 2.0" -C podman-v$VERSION --package ..
 
