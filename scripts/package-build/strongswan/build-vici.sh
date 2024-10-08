7c5c0334caac789804de1abeca52de2410310409
Viacheslav Hletenko <v.gletenko@vyos.io>
2024-10-08 08:16:48 +0000

    T6765: Fix build python3-vici package
    Fix build python3-vici. It did not include the directory
    /usr/lib/python3/dist-packages/vici

--- scripts/package-build/strongswan/build-vici.sh
+++ scripts/package-build/strongswan/build-vici.sh
@@ -4,7 +4,7 @@ set -e
 
 SRC="strongswan/src/libcharon/plugins/vici/python"
 if [ ! -d ${SRC} ]; then
-    echo "Source directory does not exists, please 'git clone'"
+    echo "Source directory does not exist, please 'git clone'"
     exit 1
 fi
 
@@ -28,30 +28,31 @@ Depends: \${misc:Depends}, \${python3:Depends}
 Description: Native Python interface for strongSwan's VICI protocol
 EOF
 
-
 # Create rules file
-echo "I: create $SRC/rules"
+echo "I: create $SRC/debian/rules"
 cat <<EOF > debian/rules
 #!/usr/bin/make -f
 
 %:
 	dh \$@ --with python3
 EOF
-# Make the rules file executable
 chmod +x debian/rules
 
 echo '10' > debian/compat
 
+# Add the 'install' file to copy the vici package to the correct directory
+echo "I: create $SRC/debian/install"
+cat <<EOF > debian/install
+vici /usr/lib/python3/dist-packages/
+EOF
+
 # Copy changelog
 cp ../../../../../debian/changelog debian/
 
-
-ls -la
-pwd
-
-
+# Build the package
 echo "I: Build Debian Package"
 dpkg-buildpackage -uc -us -tc -b -d
 
+# Copy the resulting .deb packages
 echo "I: copy packages"
-cp ../*.deb  ../../../../../../
+cp ../*.deb ../../../../../../
