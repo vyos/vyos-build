2973607c4ddb159942533f688df0d8c6bf76991f
Viacheslav <v.gletenko@vyos.io>
2025-10-16 10:52:06 +0000

    T7935: Rewrite accel-ppp-ng script to use vpp-dev binaries
    Rewrite accel-ppp-ng script to use vpp-dev binaries instead of
    building the whole VPP from sources

--- scripts/package-build/linux-kernel/build-accel-ppp-ng.sh
+++ scripts/package-build/linux-kernel/build-accel-ppp-ng.sh
@@ -3,21 +3,11 @@
 ARCH_TRIPLET=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 CWD=$(pwd)
 KERNEL_VAR_FILE=${CWD}/kernel-vars
-VPP_INCLUDE_PATH="${CWD}/../vpp/vpp/src/vpp-api:${CWD}/../vpp/vpp/src:${CWD}/../vpp/vpp/build-root/build-vpp-native/vpp/CMakeFiles/vpp-api"
-VPP_LIBRARY_PATH="${CWD}/../vpp/vpp/build-root/build-vpp-native/vpp/CMakeFiles/debian/libvppinfra/usr/lib/${ARCH_TRIPLET}:${CWD}/../vpp/vpp/build-root/install-vpp-native/vpp/lib/${ARCH_TRIPLET}"
-VPP_LIB_CHECK_PATH="${CWD}/../vpp/vpp/build-root/build-vpp-native/vpp/CMakeFiles/debian/libvppinfra/usr/lib/${ARCH_TRIPLET}"
 
 ACCEL_SRC=${CWD}/accel-ppp-ng
 
-# Build VPP as we need VPP libraries
-cd ../vpp/
-./build.py
-cd ${CWD}
-
-if [ ! -d ${VPP_LIB_CHECK_PATH} ]; then
-    echo "VPP source libraries not found"
-    exit 1
-fi
+sudo apt-get update
+sudo apt-get install -y vpp vpp-dev
 
 if [ ! -d ${ACCEL_SRC} ]; then
     echo "Accel-PPP source not found"
@@ -56,7 +46,7 @@ cmake -DBUILD_IPOE_DRIVER=TRUE \
     -DMODULES_KDIR=${KERNEL_VERSION}${KERNEL_SUFFIX} \
     -DHAVE_VPP=1 \
     -DCPACK_TYPE=Debian12 ..
-CPATH="${VPP_INCLUDE_PATH}" LIBRARY_PATH="${VPP_LIBRARY_PATH}" make
+make
 
 # Sign generated Kernel modules
 ${CWD}/sign-modules.sh .
@@ -66,5 +56,3 @@ cpack -G DEB
 # rename resulting Debian package according git description
 mv accel-ppp*.deb ${CWD}/accel-ppp-ng_$(git describe --always --tags)_$(dpkg --print-architecture).deb
 
-# move VPP binaries to linux-kernel dir, CI will get VPP .deb here
-cp ${CWD}/../vpp/*.deb ${CWD}
